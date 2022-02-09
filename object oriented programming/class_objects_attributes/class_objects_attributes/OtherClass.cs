1e88f9a9545e6a4901550c1d6cfe1b0cac632a5a
GR Silva <grscookie@gmail.com>
2022-02-09 12:12:57 -0300

    add OOP

---
+++ object oriented programming/class_objects_attributes/class_objects_attributes/OtherClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_objects_attributes
+{
+    internal class OtherClass
+    {
+        public int number;
+        public int otherNumber;
+    }
+}
