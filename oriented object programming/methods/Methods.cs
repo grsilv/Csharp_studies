928176ef842bbf9a99e302dc6c8ee01e3f4479cd
GR Silva <grscookie@gmail.com>
2022-02-12 15:25:32 -0300

    add methods

---
+++ oriented object programming/methods/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Methods
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello! Welcome!");
+        }
+
+        public void Somar(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("The result of the sum is " + result);
+        }
+
+        public void IncreaseValue(int value)
+        {
+            value += 10;
+            Console.WriteLine("Final value is " + value);
+        }
+
+        public void IncreaseRef(ref int value)
+        {
+            value += 10;
+            Console.WriteLine("Final value reference is " + value);
+        }
+
+    }
+}
---
+++ oriented object programming/methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods m1 = new Methods();
+            m1.Somar(5, 10);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            int value1 = 100;
+            int value2 = 200;
+
+            m1.IncreaseValue(value1);
+            m1.IncreaseRef(ref value2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Value: " + value1);
+            Console.WriteLine("Reference: " + value2);
+
+            Console.WriteLine("Press to next");
+            Console.ReadKey();
+        }
+    }
+}
